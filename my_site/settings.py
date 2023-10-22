36e44fe1ca449c3f5beaa3a3340f3a58750c77d4
Genoc1de <wavesshark@yandex.ru>
2023-10-22 20:12:47 +0500

    1

--- my_site/settings.py
+++ my_site/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Here
 MEDIA_URL = '/media/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
